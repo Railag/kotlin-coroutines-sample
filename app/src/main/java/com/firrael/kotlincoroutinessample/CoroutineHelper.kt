9cef757c35cd2b7c1b109204b3c1e947a58821ee
Railag <whenthegroundcavedin@gmail.com>
2021-02-09 19:53:55 +0300

    First coroutine samples

---
+++ app/src/main/java/com/firrael/kotlincoroutinessample/CoroutineHelper.kt
@@ -0,0 +1,57 @@
+package com.firrael.kotlincoroutinessample
+
+import kotlinx.coroutines.*
+import kotlin.coroutines.suspendCoroutine
+
+fun launchFirstCoroutine(delayMs: Long) {
+    println("launchFirstCoroutine")
+    GlobalScope.launch {
+        delay(delayMs)
+        println("launchFirstCoroutine delayed")
+    }
+}
+
+fun asyncCoroutine() {
+    println("start asyncCoroutine")
+
+    val deferred = (1..1_000_000).map { n ->
+        GlobalScope.async {
+            n
+        }
+    }
+
+    runBlocking {
+        val sum = deferred.sumOf { it.await().toLong() }
+        println("Sum: $sum")
+    }
+}
+
+fun callSuspendFunctionAsync(): Deferred<Int> {
+    return GlobalScope.async {
+        suspendFunction(5)
+    }
+}
+
+suspend fun suspendFunction(n: Int): Int {
+    delay(1000)
+    return n
+}
+
+fun postItem(item: String) {
+    println("postItem: $item")
+
+    GlobalScope.launch {
+        val token = preparePost()
+        val post = "post1"
+        println("Token: $token")
+        println("Post: $post")
+    }
+}
+
+suspend fun preparePost(): String {
+    // makes a request and suspends the coroutine
+    return suspendCoroutine { /* ... */ }
+}
+
+
+
--- app/src/main/java/com/firrael/kotlincoroutinessample/MainActivity.kt
+++ app/src/main/java/com/firrael/kotlincoroutinessample/MainActivity.kt
@@ -22,5 +22,16 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        launchFirstCoroutine(5000)
+        launchFirstCoroutine(1000)
+        launchFirstCoroutine(2000)
+
+        asyncCoroutine()
+
+        val deferredResult = callSuspendFunctionAsync()
+//        val result = deferredResult.getCompleted()
+
+        postItem("item1")
     }
 }
